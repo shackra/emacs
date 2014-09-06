0439518b1b34afb44515eeeb4490ab567eae75f3
shackra <elcorreo@deshackra.com>
2014-09-06 13:34:33 -0600

    eliminando momentaneamente el paquete auto-indent-mode que esta causando fallos en python-mode.

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
+ '(semantic-python-dependency-system-include-path (quote ("/usr/lib/python2.7" "/usr/lib/python2.7/plat-linux" "/usr/lib/python2.7/lib-dynload" "/usr/lib/python2.7/site-packages"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
