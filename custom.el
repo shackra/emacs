1c8e37186d428332de9720155817ca61b65d10be
shackra <elcorreo@deshackra.com>
2014-09-06 20:00:41 -0600

    movida la definición del archivo customize

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(semantic-python-dependency-system-include-path (quote ("/usr/lib/python2.7" "/usr/lib/python2.7/plat-linux" "/usr/lib/python2.7/lib-dynload" "/usr/lib/python2.7/site-packages"))))
+ '(elpy-modules (quote (elpy-module-company elpy-module-eldoc elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
