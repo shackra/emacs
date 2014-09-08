ee1070000fa32416b98e6fa5eeb691cdea0ca2cc
shackra <elcorreo@deshackra.com>
2014-09-08 09:32:55 -0600

    el archivo custom.el carga antes, todo funciona ahora

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(elpy-interactive-python-command "ipython")
  '(elpy-modules (quote (elpy-module-company elpy-module-eldoc elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults)))
+ '(elpy-rpc-backend "jedi")
  '(elpy-rpc-python-command "python2")
  '(magit-use-overlays nil))
 (custom-set-faces
