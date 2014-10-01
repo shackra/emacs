4eaf85a14d8a3df1fa894f9221917d761749d503
shackra <elcorreo@deshackra.com>
2014-09-30 19:22:08 -0600

    cambios en org-mode y cambio de tema

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(custom-safe-themes (quote ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(elpy-interactive-python-command "ipython")
  '(elpy-modules (quote (elpy-module-company elpy-module-eldoc elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults)))
  '(elpy-rpc-backend "jedi")
@@ -14,6 +14,8 @@
  '(magit-use-overlays nil)
  '(py-autopep8-options (quote ("--ignore E711,E712,E24" "--aggressive --aggressive --aggressive")))
  '(safe-local-variable-values (quote ((encoding . utf-8))))
+ '(tramp-default-method "ssh")
+ '(tramp-encoding-shell "/bin/bash")
  '(vr/default-replace-preview t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- init.el
+++ init.el
@@ -4,3 +4,4 @@
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
 		   user-emacs-directory))
+(put 'upcase-region 'disabled nil)
