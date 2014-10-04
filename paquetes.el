c95ef9c067a8d93bb85bc3a663c1d28cd9f96eeb
shackra <elcorreo@deshackra.com>
2014-10-04 12:22:38 -0600

    se usa use-package para asegurarse que los paquetes estan instalados en la carpeta elpa del usuario

--- paquetes.el
+++ paquetes.el
@@ -1,95 +1,174 @@
-;; Install extensions if they're missing
-(defun init--install-packages ()
-  (packages-install
-   '(ace-jump-buffer
-     ace-jump-mode
-     ace-window
-     async
-     autopair
-     back-button
-     button-lock
-     cmake-mode
-     cmake-project
-     company
-     company-c-headers
-     company-cmake
-     company-irony
-     dash
-     diminish
-     dired+
-     discover-my-major
-     dpaste
-     elpy
-     epl
-     expand-region
-     f
-     find-file-in-project
-     fixmee
-     flycheck
-     flycheck-rust
-     git-commit-mode
-     git-rebase-mode
-     go-eldoc
-     go-mode
-     helm
-     helm-descbinds
-     helm-projectile
-     helm-swoop
-     highlight-blocks
-     highlight-escape-sequences
-     highlight-indentation
-     highlight-numbers
-     hlinum
-     ht
-     htmlize
-     idomenu
-     imenu+
-     irony
-     kivy-mode
-     ledger-mode
-     lice
-     list-utils
-     magit
-     makey
-     monky
-     monokai-theme
-     multi-term
-     multiple-cursors
-     mustache
-     nav-flash
-     nlinum
-     org
-     org-page
-     pandoc-mode
-     parent-mode
-     pcache
-     persistent-soft
-     pkg-info
-     powerline
-     pretty-mode
-     pretty-mode-plus
-     projectile
-     py-autopep8
-     pyvenv
-     rainbow-mode
-     relative-buffers
-     rich-minority
-     rust-mode
-     s
-     smart-mode-line
-     smartrep
-     string-utils
-     ucs-utils
-     visual-regexp
-     visual-regexp-steroids
-     web-beautify
-     web-mode
-     yaml-mode
-     yasnippet)))
-
-(condition-case nil
-    (init--install-packages)
-  (error
-   (package-refresh-contents)
-   (init--install-packages)))
-
+(use-package ace-jump-buffer
+  :ensure ace-jump-buffer)
+(use-package ace-jump-mode
+  :ensure ace-jump-mode)
+(use-package ace-window
+  :ensure ace-window)
+(use-package async
+  :ensure async)
+(use-package autopair
+  :ensure autopair)
+(use-package back-button
+  :ensure back-button)
+(use-package bind-key
+  :ensure bind-key)
+(use-package button-lock
+  :ensure button-lock)
+(use-package cmake-mode
+  :ensure cmake-mode)
+(use-package cmake-project
+  :ensure cmake-project)
+(use-package company
+  :ensure company)
+(use-package company-c-headers
+  :ensure company-c-headers)
+(use-package company-cmake
+  :ensure company-cmake)
+(use-package company-irony
+  :ensure company-irony)
+(use-package dash
+  :ensure dash)
+(use-package diminish
+  :ensure diminish)
+(use-package dired+
+  :ensure dired+)
+(use-package discover-my-major
+  :ensure discover-my-major)
+(use-package dpaste
+  :ensure dpaste)
+(use-package elpy
+  :ensure elpy)
+(use-package epl
+  :ensure epl)
+(use-package expand-region
+  :ensure expand-region)
+(use-package f
+  :ensure f)
+(use-package find-file-in-project
+  :ensure find-file-in-project)
+(use-package fixmee
+  :ensure fixmee)
+(use-package flycheck
+  :ensure flycheck)
+(use-package flycheck-rust
+  :ensure flycheck-rust)
+(use-package git-commit-mode
+  :ensure git-commit-mode)
+(use-package git-rebase-mode
+  :ensure git-rebase-mode)
+(use-package go-eldoc
+  :ensure go-eldoc)
+(use-package go-mode
+  :ensure go-mode)
+(use-package helm
+  :ensure helm)
+(use-package helm-descbinds
+  :ensure helm-descbinds)
+(use-package helm-projectile
+  :ensure helm-projectile)
+(use-package helm-swoop
+  :ensure helm-swoop)
+(use-package highlight-blocks
+  :ensure highlight-blocks)
+(use-package highlight-escape-sequences
+  :ensure highlight-escape-sequences)
+(use-package highlight-indentation
+  :ensure highlight-indentation)
+(use-package highlight-numbers
+  :ensure highlight-numbers)
+(use-package hlinum
+  :ensure hlinum)
+(use-package ht
+  :ensure ht)
+(use-package htmlize
+  :ensure htmlize)
+(use-package idomenu
+  :ensure idomenu)
+(use-package imenu+
+  :ensure imenu+)
+(use-package irony
+  :ensure irony)
+(use-package kivy-mode
+  :ensure kivy-mode)
+(use-package ledger-mode
+  :ensure ledger-mode)
+(use-package lice
+  :ensure lice)
+(use-package list-utils
+  :ensure list-utils)
+(use-package magit
+  :ensure magit)
+(use-package makey
+  :ensure makey)
+(use-package monky
+  :ensure monky)
+(use-package monokai-theme
+  :ensure monokai-theme)
+(use-package multi-term
+  :ensure multi-term)
+(use-package multiple-cursors
+  :ensure multiple-cursors)
+(use-package mustache
+  :ensure mustache)
+(use-package nav-flash
+  :ensure nav-flash)
+(use-package nlinum
+  :ensure nlinum)
+(use-package org
+  :ensure org)
+(use-package org-page
+  :ensure org-page)
+(use-package pandoc-mode
+  :ensure pandoc-mode)
+(use-package parent-mode
+  :ensure parent-mode)
+(use-package pcache
+  :ensure pcache)
+(use-package persistent-soft
+  :ensure persistent-soft)
+(use-package pkg-info
+  :ensure pkg-info)
+(use-package powerline
+  :ensure powerline)
+(use-package pretty-mode
+  :ensure pretty-mode)
+(use-package pretty-mode-plus
+  :ensure pretty-mode-plus)
+(use-package projectile
+  :ensure projectile)
+(use-package py-autopep8
+  :ensure py-autopep8)
+(use-package pyvenv
+  :ensure pyvenv)
+(use-package rainbow-mode
+  :ensure rainbow-mode)
+(use-package relative-buffers
+  :ensure relative-buffers)
+(use-package rich-minority
+  :ensure rich-minority)
+(use-package rust-mode
+  :ensure rust-mode)
+(use-package s
+  :ensure s)
+(use-package smart-mode-line
+  :ensure smart-mode-line)
+(use-package smartrep
+  :ensure smartrep)
+(use-package string-utils
+  :ensure string-utils)
+(use-package ucs-utils
+  :ensure ucs-utils)
+(use-package use-package
+  :ensure use-package)
+(use-package visual-regexp
+  :ensure visual-regexp)
+(use-package visual-regexp-steroids
+  :ensure visual-regexp-steroids)
+(use-package web-beautify
+  :ensure web-beautify)
+(use-package web-mode
+  :ensure web-mode)
+(use-package yaml-mode
+  :ensure yaml-mode)
+(use-package yasnippet
+  :ensure yasnippet)
