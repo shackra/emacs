17a7fbd566415811b00f9f508322aac7eaa495ca
shackra <elcorreo@deshackra.com>
2014-11-03 13:57:58 -0600

    varios arreglos en la configuración

--- custom.el
+++ custom.el
@@ -5,10 +5,36 @@
  ;; If there is more than one, they won't work right.
  '(calendar-christian-all-holidays-flag t)
  '(calendar-date-style (quote european))
- '(calendar-holidays (quote ((holiday-fixed 1 1 "Año nuevo") (holiday-fixed 2 14 "San Valentin") (holiday-fixed 8 15 "Día de la Madre") (holiday-float 6 0 3 "Día del Padre") (holiday-fixed 9 15 "Día de la Independencia") (holiday-fixed 5 1 "Día Internacional del Trabajo") (holiday-fixed 7 25 "Anexión del Partido de Nicoya a Costa Rica") (holiday-fixed 8 2 "Día de la Virgen de los Ángeles") (holiday-fixed 9 12 "Día de las Culturas") (holiday-fixed 10 31 "Víspera de Todos los Santos") (holiday-fixed 11 19 "Día Internacional del Hombre") (holiday-easter-etc) (holiday-fixed 12 25 "Navidad") (if calendar-christian-all-holidays-flag (append (holiday-fixed 1 6 "Epifanía") (holiday-julian 12 25 "Navidad (calendario Juleano)") (holiday-greek-orthodox-easter) (holiday-fixed 8 15 "Asunción de María") (holiday-advent 0 "Adviento"))) (solar-equinoxes-solstices))))
- '(custom-safe-themes (quote ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(calendar-holidays
+   (quote
+    ((holiday-fixed 1 1 "Año nuevo")
+     (holiday-fixed 2 14 "San Valentin")
+     (holiday-fixed 8 15 "Día de la Madre")
+     (holiday-float 6 0 3 "Día del Padre")
+     (holiday-fixed 9 15 "Día de la Independencia")
+     (holiday-fixed 5 1 "Día Internacional del Trabajo")
+     (holiday-fixed 7 25 "Anexión del Partido de Nicoya a Costa Rica")
+     (holiday-fixed 8 2 "Día de la Virgen de los Ángeles")
+     (holiday-fixed 9 12 "Día de las Culturas")
+     (holiday-fixed 10 31 "Víspera de Todos los Santos")
+     (holiday-fixed 11 19 "Día Internacional del Hombre")
+     (holiday-easter-etc)
+     (holiday-fixed 12 25 "Navidad")
+     (if calendar-christian-all-holidays-flag
+         (append
+          (holiday-fixed 1 6 "Epifanía")
+          (holiday-julian 12 25 "Navidad (calendario Juleano)")
+          (holiday-greek-orthodox-easter)
+          (holiday-fixed 8 15 "Asunción de María")
+          (holiday-advent 0 "Adviento")))
+     (solar-equinoxes-solstices))))
+ '(custom-safe-themes
+   (quote
+    ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(elpy-interactive-python-command "ipython")
- '(elpy-modules (quote (elpy-module-company elpy-module-eldoc elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults)))
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults)))
  '(elpy-rpc-backend "jedi")
  '(elpy-rpc-python-command "python2")
  '(flycheck-check-syntax-automatically (quote (save idle-change new-line mode-enabled)))
@@ -16,15 +42,40 @@
  '(flycheck-idle-change-delay 2.0)
  '(flycheck-indication-mode (quote right-fringe))
  '(global-flycheck-mode t nil (flycheck))
- '(holiday-christian-holidays (quote ((holiday-easter-etc) (holiday-fixed 12 25 "Navidad") (if calendar-christian-all-holidays-flag (append (holiday-fixed 1 6 "Epifanía") (holiday-julian 12 25 "Navidad (calendario Juleano)") (holiday-greek-orthodox-easter) (holiday-fixed 8 15 "Asunción de María") (holiday-advent 0 "Adviento"))))))
- '(holiday-general-holidays (quote ((holiday-fixed 1 1 "Año nuevo") (holiday-fixed 2 14 "San Valentin") (holiday-fixed 8 15 "Día de la Madre") (holiday-float 6 0 3 "Día del Padre") (holiday-fixed 9 15 "Día de la Independencia") (holiday-fixed 5 1 "Día Internacional del Trabajo") (holiday-fixed 7 25 "Anexión del Partido de Nicoya a Costa Rica") (holiday-fixed 8 2 "Día de la Virgen de los Ángeles") (holiday-fixed 9 12 "Día de las Culturas") (holiday-fixed 10 31 "Víspera de Todos los Santos"))))
- '(holiday-other-holidays (quote ((holiday-fixed 11 19 "Día Internacional del Hombre"))))
+ '(holiday-christian-holidays
+   (quote
+    ((holiday-easter-etc)
+     (holiday-fixed 12 25 "Navidad")
+     (if calendar-christian-all-holidays-flag
+         (append
+          (holiday-fixed 1 6 "Epifanía")
+          (holiday-julian 12 25 "Navidad (calendario Juleano)")
+          (holiday-greek-orthodox-easter)
+          (holiday-fixed 8 15 "Asunción de María")
+          (holiday-advent 0 "Adviento"))))))
+ '(holiday-general-holidays
+   (quote
+    ((holiday-fixed 1 1 "Año nuevo")
+     (holiday-fixed 2 14 "San Valentin")
+     (holiday-fixed 8 15 "Día de la Madre")
+     (holiday-float 6 0 3 "Día del Padre")
+     (holiday-fixed 9 15 "Día de la Independencia")
+     (holiday-fixed 5 1 "Día Internacional del Trabajo")
+     (holiday-fixed 7 25 "Anexión del Partido de Nicoya a Costa Rica")
+     (holiday-fixed 8 2 "Día de la Virgen de los Ángeles")
+     (holiday-fixed 9 12 "Día de las Culturas")
+     (holiday-fixed 10 31 "Víspera de Todos los Santos"))))
+ '(holiday-other-holidays
+   (quote
+    ((holiday-fixed 11 19 "Día Internacional del Hombre"))))
  '(magit-use-overlays nil)
  '(org-babel-load-languages (quote ((ledger . t) (python . t) (emacs-lisp . t))))
- '(py-autopep8-options (quote ("--ignore E711,E712,E24" "--aggressive --aggressive --aggressive")))
+ '(py-autopep8-options
+   (quote
+    ("--ignore E711,E712,E24" "--aggressive --aggressive --aggressive")))
  '(python-shell-interpreter "python2")
  '(pyvenv-virtualenvwrapper-python "/usr/bin/python2")
- '(safe-local-variable-values (quote ((encoding . utf-8))))
+ '(safe-local-variable-values (quote ((ispell-dictionary . en) (encoding . utf-8))))
  '(tramp-default-method "ssh")
  '(tramp-encoding-shell "/bin/bash")
  '(vr/default-replace-preview t))
--- paquetes.el
+++ paquetes.el
@@ -1,196 +1,202 @@
-(use-package ace-jump-buffer
-  :ensure ace-jump-buffer)
-(use-package ace-jump-mode
-  :ensure ace-jump-mode)
-(use-package ace-window
-  :ensure ace-window)
-(use-package async
-  :ensure async)
-(use-package autopair
-  :ensure autopair)
-(use-package back-button
-  :ensure back-button)
-(use-package bind-key
-  :ensure bind-key)
-(use-package button-lock
-  :ensure button-lock)
-(use-package cmake-mode
-  :ensure cmake-mode)
-(use-package cmake-project
-  :ensure cmake-project)
-(use-package company
-  :ensure company)
-(use-package company-c-headers
-  :ensure company-c-headers)
-(use-package company-cmake
-  :ensure company-cmake)
-(use-package company-irony
-  :ensure company-irony)
-(use-package connection
-  :ensure connection)
-(use-package dash
-  :ensure dash)
-(use-package diminish
-  :ensure diminish)
-(use-package dired+
-  :ensure dired+)
-(use-package discover-my-major
-  :ensure discover-my-major)
-(use-package dpaste
-  :ensure dpaste)
-(use-package elpy
-  :ensure elpy)
-(use-package epl
-  :ensure epl)
-(use-package expand-region
-  :ensure expand-region)
-(use-package f
-  :ensure f)
-(use-package find-file-in-project
-  :ensure find-file-in-project)
-(use-package fixmee
-  :ensure fixmee)
-(use-package flycheck
-  :ensure flycheck)
-(use-package flycheck-rust
-  :ensure flycheck-rust)
-(use-package git-commit-mode
-  :ensure git-commit-mode)
-(use-package git-rebase-mode
-  :ensure git-rebase-mode)
-(use-package go-eldoc
-  :ensure go-eldoc)
-(use-package go-mode
-  :ensure go-mode)
-(use-package helm
-  :ensure helm)
-(use-package helm-descbinds
-  :ensure helm-descbinds)
-(use-package helm-projectile
-  :ensure helm-projectile)
-(use-package helm-swoop
-  :ensure helm-swoop)
-(use-package highlight-blocks
-  :ensure highlight-blocks)
-(use-package highlight-escape-sequences
-  :ensure highlight-escape-sequences)
-(use-package highlight-indentation
-  :ensure highlight-indentation)
-(use-package highlight-numbers
-  :ensure highlight-numbers)
-(use-package hlinum
-  :ensure hlinum)
-(use-package ht
-  :ensure ht)
-(use-package htmlize
-  :ensure htmlize)
-(use-package idomenu
-  :ensure idomenu)
-(use-package imenu+
-  :ensure imenu+)
-(use-package irony
-  :ensure irony)
-(use-package jabber
-  :ensure jabber)
-(use-package kivy-mode
-  :ensure kivy-mode)
-(use-package langtool
-  :ensure langtool)
-(use-package ledger-mode
-  :ensure ledger-mode)
-(use-package lice
-  :ensure lice)
-(use-package link
-  :ensure link)
-(use-package list-utils
-  :ensure list-utils)
-(use-package magit
-  :ensure magit)
-(use-package makey
-  :ensure makey)
-(use-package markdown-mode
-  :ensure markdown-mode)
-(use-package markdown-mode+
-  :ensure markdown-mode+)
-(use-package monky
-  :ensure monky)
-(use-package monokai-theme
-  :ensure monokai-theme)
-(use-package multi-term
-  :ensure multi-term)
-(use-package multiple-cursors
-  :ensure multiple-cursors)
-(use-package muse
-  :ensure muse)
-(use-package mustache
-  :ensure mustache)
-(use-package nav-flash
-  :ensure nav-flash)
-(use-package nlinum
-  :ensure nlinum)
-(use-package notify
-  :ensure notify)
-(use-package org
-  :ensure org)
-(use-package org-page
-  :ensure org-page)
-(use-package pandoc-mode
-  :ensure pandoc-mode)
-(use-package parent-mode
-  :ensure parent-mode)
-(use-package pcache
-  :ensure pcache)
-(use-package persistent-soft
-  :ensure persistent-soft)
-(use-package pkg-info
-  :ensure pkg-info)
-(use-package pkgbuild-mode
-  :ensure pkgbuild-mode)
-(use-package pony-mode
-  :ensure pony-mode)
-(use-package powerline
-  :ensure powerline)
-(use-package pretty-mode
-  :ensure pretty-mode)
-(use-package pretty-mode-plus
-  :ensure pretty-mode-plus)
-(use-package projectile
-  :ensure projectile)
-(use-package py-autopep8
-  :ensure py-autopep8)
-(use-package pyvenv
-  :ensure pyvenv)
-(use-package rainbow-mode
-  :ensure rainbow-mode)
-(use-package relative-buffers
-  :ensure relative-buffers)
-(use-package rich-minority
-  :ensure rich-minority)
-(use-package rust-mode
-  :ensure rust-mode)
-(use-package s
-  :ensure s)
-(use-package smart-mode-line
-  :ensure smart-mode-line)
-(use-package smartrep
-  :ensure smartrep)
-(use-package string-utils
-  :ensure string-utils)
-(use-package ucs-utils
-  :ensure ucs-utils)
-(use-package undo-tree
-  :ensure undo-tree)
-(use-package use-package
-  :ensure use-package)
-(use-package visual-regexp
-  :ensure visual-regexp)
-(use-package visual-regexp-steroids
-  :ensure visual-regexp-steroids)
-(use-package web-beautify
-  :ensure web-beautify)
-(use-package web-mode
-  :ensure web-mode)
-(use-package yaml-mode
-  :ensure yaml-mode)
-(use-package yasnippet
-  :ensure yasnippet)
+(ignore-errors (use-package ace-jump-buffer
+  :ensure ace-jump-buffer))
+(ignore-errors (use-package ace-jump-mode
+  :ensure ace-jump-mode))
+(ignore-errors (use-package ace-window
+  :ensure ace-window))
+(ignore-errors (use-package async
+  :ensure async))
+(ignore-errors (use-package autopair
+  :ensure autopair))
+(ignore-errors (use-package back-button
+  :ensure back-button))
+(ignore-errors (use-package bind-key
+  :ensure bind-key))
+(ignore-errors (use-package button-lock
+  :ensure button-lock))
+(ignore-errors (use-package cmake-mode
+  :ensure cmake-mode))
+(ignore-errors (use-package cmake-project
+  :ensure cmake-project))
+(ignore-errors (use-package company
+  :ensure company))
+(ignore-errors (use-package company-c-headers
+  :ensure company-c-headers))
+(ignore-errors (use-package company-cmake
+  :ensure company-cmake))
+(ignore-errors (use-package company-irony
+  :ensure company-irony))
+(ignore-errors (use-package connection
+  :ensure connection))
+(ignore-errors (use-package dash
+  :ensure dash))
+(ignore-errors (use-package diminish
+  :ensure diminish))
+(ignore-errors (use-package dired+
+  :ensure dired+))
+(ignore-errors (use-package discover-my-major
+  :ensure discover-my-major))
+(ignore-errors (use-package dpaste
+  :ensure dpaste))
+(ignore-errors (use-package elpy
+  :ensure elpy))
+(ignore-errors (use-package epl
+  :ensure epl))
+(ignore-errors (use-package expand-region
+  :ensure expand-region))
+(ignore-errors (use-package eyebrowse
+  :ensure eyebrowse))
+(ignore-errors (use-package f
+  :ensure f))
+(ignore-errors (use-package find-file-in-project
+  :ensure find-file-in-project))
+(ignore-errors (use-package fixmee
+  :ensure fixmee))
+(ignore-errors (use-package flycheck
+  :ensure flycheck))
+(ignore-errors (use-package flycheck-rust
+  :ensure flycheck-rust))
+(ignore-errors (use-package git-commit-mode
+  :ensure git-commit-mode))
+(ignore-errors (use-package git-rebase-mode
+  :ensure git-rebase-mode))
+(ignore-errors (use-package gnup
+  :ensure gnup))
+(ignore-errors (use-package go-eldoc
+  :ensure go-eldoc))
+(ignore-errors (use-package go-mode
+  :ensure go-mode))
+(ignore-errors (use-package helm
+  :ensure helm))
+(ignore-errors (use-package helm-descbinds
+  :ensure helm-descbinds))
+(ignore-errors (use-package helm-projectile
+  :ensure helm-projectile))
+(ignore-errors (use-package helm-swoop
+  :ensure helm-swoop))
+(ignore-errors (use-package highlight-blocks
+  :ensure highlight-blocks))
+(ignore-errors (use-package highlight-escape-sequences
+  :ensure highlight-escape-sequences))
+(ignore-errors (use-package highlight-indentation
+  :ensure highlight-indentation))
+(ignore-errors (use-package highlight-numbers
+  :ensure highlight-numbers))
+(ignore-errors (use-package hlinum
+  :ensure hlinum))
+(ignore-errors (use-package ht
+  :ensure ht))
+(ignore-errors (use-package htmlize
+  :ensure htmlize))
+(ignore-errors (use-package idomenu
+  :ensure idomenu))
+(ignore-errors (use-package imenu+
+  :ensure imenu+))
+(ignore-errors (use-package irony
+  :ensure irony))
+(ignore-errors (use-package jabber
+  :ensure jabber))
+(ignore-errors (use-package js2-mode
+  :ensure js2-mode))
+(ignore-errors (use-package kivy-mode
+  :ensure kivy-mode))
+(ignore-errors (use-package langtool
+  :ensure langtool))
+(ignore-errors (use-package ledger-mode
+  :ensure ledger-mode))
+(ignore-errors (use-package lice
+  :ensure lice))
+(ignore-errors (use-package link
+  :ensure link))
+(ignore-errors (use-package list-utils
+  :ensure list-utils))
+(ignore-errors (use-package magit
+  :ensure magit))
+(ignore-errors (use-package makey
+  :ensure makey))
+(ignore-errors (use-package markdown-mode
+  :ensure markdown-mode))
+(ignore-errors (use-package markdown-mode+
+  :ensure markdown-mode+))
+(ignore-errors (use-package monky
+  :ensure monky))
+(ignore-errors (use-package monokai-theme
+  :ensure monokai-theme))
+(ignore-errors (use-package multi-term
+  :ensure multi-term))
+(ignore-errors (use-package multiple-cursors
+  :ensure multiple-cursors))
+(ignore-errors (use-package muse
+  :ensure muse))
+(ignore-errors (use-package mustache
+  :ensure mustache))
+(ignore-errors (use-package nav-flash
+  :ensure nav-flash))
+(ignore-errors (use-package nlinum
+  :ensure nlinum))
+(ignore-errors (use-package notify
+  :ensure notify))
+(ignore-errors (use-package org
+  :ensure org))
+(ignore-errors (use-package org-page
+  :ensure org-page))
+(ignore-errors (use-package pandoc-mode
+  :ensure pandoc-mode))
+(ignore-errors (use-package parent-mode
+  :ensure parent-mode))
+(ignore-errors (use-package pcache
+  :ensure pcache))
+(ignore-errors (use-package persistent-soft
+  :ensure persistent-soft))
+(ignore-errors (use-package pkg-info
+  :ensure pkg-info))
+(ignore-errors (use-package pkgbuild-mode
+  :ensure pkgbuild-mode))
+(ignore-errors (use-package pony-mode
+  :ensure pony-mode))
+(ignore-errors (use-package powerline
+  :ensure powerline))
+(ignore-errors (use-package pretty-mode
+  :ensure pretty-mode))
+(ignore-errors (use-package pretty-mode-plus
+  :ensure pretty-mode-plus))
+(ignore-errors (use-package projectile
+  :ensure projectile))
+(ignore-errors (use-package py-autopep8
+  :ensure py-autopep8))
+(ignore-errors (use-package pyvenv
+  :ensure pyvenv))
+(ignore-errors (use-package rainbow-mode
+  :ensure rainbow-mode))
+(ignore-errors (use-package relative-buffers
+  :ensure relative-buffers))
+(ignore-errors (use-package rich-minority
+  :ensure rich-minority))
+(ignore-errors (use-package rust-mode
+  :ensure rust-mode))
+(ignore-errors (use-package s
+  :ensure s))
+(ignore-errors (use-package smart-mode-line
+  :ensure smart-mode-line))
+(ignore-errors (use-package smartrep
+  :ensure smartrep))
+(ignore-errors (use-package string-utils
+  :ensure string-utils))
+(ignore-errors (use-package ucs-utils
+  :ensure ucs-utils))
+(ignore-errors (use-package undo-tree
+  :ensure undo-tree))
+(ignore-errors (use-package use-package
+  :ensure use-package))
+(ignore-errors (use-package visual-regexp
+  :ensure visual-regexp))
+(ignore-errors (use-package visual-regexp-steroids
+  :ensure visual-regexp-steroids))
+(ignore-errors (use-package web-beautify
+  :ensure web-beautify))
+(ignore-errors (use-package web-mode
+  :ensure web-mode))
+(ignore-errors (use-package yaml-mode
+  :ensure yaml-mode))
+(ignore-errors (use-package yasnippet
+  :ensure yasnippet))
