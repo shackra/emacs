91af175e00ed31c3c8be77b37506f116e2a59a88
Jorge Araya Navarro <elcorreo@deshackra.com>
2015-03-03 01:36:11 -0600

    La fecha de modificación de los archivos org y elisp son verificados para indicarle a Emacs qué hacer si uno es más nuevo que otro o el otro no existe

--- init.el
+++ init.el
@@ -1,8 +1,12 @@
 ;; init.el para esta configuración. Tu Emacs debe ser versión 24.
 (package-initialize)
 (require 'ob-tangle)
-(org-babel-load-file
- (expand-file-name "emacs-init.org"
-		   user-emacs-directory))
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
+;; cuando el archivo org es más reciente que el archivo elisp, el archivo elisp
+;; se recrea a partir de los bloques de código en el archivo org. Esto deberia
+;; ahorrarme unos segundos cuando Emacs se carga.
+(if (file-newer-than-file-p (expand-file-name "emacs-init.org" user-emacs-directory)
+                            (expand-file-name "emacs-init.el" user-emacs-directory))
+    ;; enreda los bloques de código en un archivo elisp
+    (org-babel-load-file (expand-file-name "emacs-init.org" user-emacs-directory))
+  ;; en caso contrario, carga el archivo ya existente :)
+  (load-file (expand-file-name "emacs-init.el" user-emacs-directory)))
