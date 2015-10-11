748d8e0883d6133c4677d78b1bad8dc14b7e2809
Jorge Araya Navarro <elcorreo@deshackra.com>
2015-10-10 19:55:02 -0600

    Varias secciones migradas, incluyendo helm

--- init.el
+++ init.el
@@ -1,5 +1,23 @@
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Packaging-Basics.html
+(setf package-enable-at-startup nil)
 ;; init.el para esta configuración. Tu Emacs debe ser versión 24.
 (package-initialize)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; Para importes librerias que requieren compatibilidad como cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+;; revisamos si no tenemos use-package instalado, porque de ser verdadero esto,
+;; lo instalamos
+(when (not (package-installed-p 'use-package))
+  (progn
+    (unless package-archive-contents
+      (package-refresh-contents))
+    (package-install 'use-package)))
+
+(require 'use-package)
+(setf use-package-always-ensure t)
 (require 'ob-tangle)
 ;; cuando el archivo org es más reciente que el archivo elisp, el archivo elisp
 ;; se recrea a partir de los bloques de código en el archivo org. Esto deberia
