a6074110b84191500493902d96e8837b9b4097c6
Jorge Araya Navarro <elcorreo@deshackra.com>
2016-06-23 19:51:12 -0600

    org-refile varios archivos y plantilla para Elisp

--- custom.el
+++ custom.el
@@ -145,3 +145,9 @@
      (360 . "#8bc34a"))))
  '(vc-annotate-very-old-color nil)
  '(vr/default-replace-preview t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:foreground "#8D6E63" :weight bold :height 1.5)))))
