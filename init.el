9f3ded5877b8339743e1ec9df6be045b0bbcf29a
Jorge Araya Navarro <jorge@esavara.cr>
2017-09-26 14:35:51 -0600

    new: Actualiza preparativos para package.el
    https://github.com/s-kostyaev/.emacs.d/blob/5c872864533749879fc7d1008a29fc68833ee04c/init.el#L10

--- init.el
+++ init.el
@@ -3,12 +3,25 @@
   "Tiempo en que Emacs arrancó")
 
 (setq load-prefer-newer t)
-;; This sets up the load path so that we can override it
-(setf package-enable-at-startup nil)
+;; Disable package initialize after us.  We either initialize it
+;; anyway in case of interpreted .emacs, or we don't want slow
+;; initialization in case of byte-compiled .emacs.elc.
+(setq package-enable-at-startup nil)
+;; Ask package.el to not add (package-initialize) to .emacs.
+(setq package--init-file-ensured t)
+;; set use-package-verbose to t for interpreted .emacs,
+;; and to nil for byte-compiled .emacs.elc
+(eval-and-compile
+  (setq use-package-verbose (not (bound-and-true-p byte-compile-current-file))))
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Packaging-Basics.html
 (package-initialize nil)
 
 (setq inhibit-startup-screen t)
+;; :pin no funciona en use-package por el momento, entonces si deseamos que se
+;; instale un paquete desde el repositorio que queremos debemos especificarlo
+;; aqui antes de que use-package sea inicializado
+(setq package-pinned-packages '((auctex . "gnu")
+                                (python . "gnu")))
 
 (defun my-tangle-section-canceled ()
   "Checks if the previous section header was CANC"
