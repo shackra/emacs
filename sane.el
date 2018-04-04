ba6d1662ffeb8832e96e60f355522e6b44b7a0bd
Jorge Araya Navarro <jorge@esavara.cr>
2018-04-04 00:59:50 -0600

    chg: Agrega opciones para utf-8 como sistema de codificado

--- sane.el
+++ sane.el
@@ -37,10 +37,12 @@
 (set-default 'indicate-empty-lines t)
 (setq-default truncate-lines t)
 
-(prefer-coding-system 'utf-8)
+(prefer-coding-system       'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
 (if
     (boundp 'buffer-file-coding-system)
