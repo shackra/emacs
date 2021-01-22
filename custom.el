ff1dededb2b1e6327479a0b434f0dfa5e0dbe7c8
Jorge Javier Araya Navarro <jorge@esavara.cr>
2021-01-22 13:32:03 -0600

    fix: Arregla los archivos de agendas

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" default)))
+   '("d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" default))
+ '(safe-local-variable-values
+   '((org-download-image-dir . "./imagenes")
+     (org-clock-mode-line-total . today))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
