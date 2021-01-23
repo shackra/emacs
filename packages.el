42c0a8422aa69d448dfabf6f10af5bc45b1b8c9f
Jorge Araya <joaraya@tradestation.com>
2021-01-22 18:11:50 -0600

    chg: Define formateador con prettier usando npx

--- packages.el
+++ packages.el
@@ -1,8 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! reformatter)
-
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
