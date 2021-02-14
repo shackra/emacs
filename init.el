7b32b1e4242479e286f17df488b8c85e7a16ab06
Jorge Javier Araya Navarro <jorge@esavara.cr>
2021-02-14 14:40:20 -0600

    chg: Extiende complementos para org

--- init.el
+++ init.el
@@ -150,7 +150,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +dragndrop +pretty)         ; organize your plain life in plain text
+       (org +dragndrop +pretty +noter +pomodoro +present)         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
