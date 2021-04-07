ef6779f786648f438461249b9779d99120d71b3c
Jorge Javier Araya Navarro <jorge@esavara.cr>
2021-04-06 18:41:13 -0600

    chg: Cambia repo fuente para tree-sitter-langs

--- packages.el
+++ packages.el
@@ -27,10 +27,10 @@
   :disable (not (functionp 'module-load)))
 
 (package! tree-sitter-langs
-  :recipe (:host github :repo "ubolonton/emacs-tree-sitter"
-           :files ("langs/*.el"
-                   "langs/queries"))
-  :pin "3a600d769bd5da95bf46bec58893934370c6c04f"
+  :recipe (:host github :repo "ubolonton/tree-sitter-langs"
+           :files ("*.el"
+                   "queries/"))
+  :pin "5d362ce98dcf656d7a55fcad6ae21c0a2caca861"
   :disable (not (functionp 'module-load)))
 
 (package! tsc
