1a3681b506e40856ad98273555cef4bab14480a1
Jorge Javier Araya Navarro <jorge@esavara.cr>
2021-06-24 20:17:08 -0600

    fix: Elimina custom-set-faces de custom.el

--- custom.el
+++ custom.el
@@ -9,9 +9,3 @@
    '((git-commit-major-mode . git-commit-elisp-text-mode)
      (org-download-image-dir . "./imagenes")
      (org-clock-mode-line-total . today))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:foreground "white" :background "red" :weight bold :height 2.5 :box (:line-width 10 :color "red"))))))
