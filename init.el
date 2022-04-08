c2e18b853a9dcc6dcb1b3f5bda2e52009d17910c
Jorge Araya <joaraya@tradestation.com>
2022-04-07 20:31:56 -0600

    new: Crea plantilla para Golang

--- init.el
+++ init.el
@@ -79,7 +79,7 @@
 
  :checkers
  syntax                                             ; tasing you for every semicolon you forget
- (spell +enchant +everywhere)                       ; tasing you for misspelling mispelling
+ (spell +aspell +everywhere)                        ; tasing you for misspelling mispelling
  grammar                                            ; tasing grammar mistake every you make
 
  :tools
