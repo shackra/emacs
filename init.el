e1757fc574968b47e74924b59ce05bb92a2eb577
Jorge Javier Araya Navarro <jorge@esavara.cr>
2022-06-21 15:56:16 -0600

    new: Habilita Haskell

--- init.el
+++ init.el
@@ -129,7 +129,7 @@
  ;;fstar                                            ; (dependent) types and (monadic) effects and Z3
  (gdscript +lsp)                                    ; the language you waited for
  (go +lsp)                                          ; the hipster dialect
- ;;(haskell +dante)                                 ; a language that's lazier than I am
+ (haskell +dante +lsp)                              ; a language that's lazier than I am
  ;;hy                                               ; readability of scheme w/ speed of python
  ;;idris                                            ; a language you can depend on
  json                                               ; At least it ain't XML
