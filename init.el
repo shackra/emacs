f72e6d19e2bc46db9e3b2477ea01131169eb9719
Jorge Javier Araya Navarro <jorge@esavara.cr>
2022-07-07 10:10:09 -0600

    fix: Utiliza golangci-lint en Go por defecto

--- init.el
+++ init.el
@@ -78,7 +78,7 @@
  vterm                                              ; the best terminal emulation in Emacs
 
  :checkers
- syntax                                             ; tasing you for every semicolon you forget
+ (syntax +childframe)                               ; tasing you for every semicolon you forget
  (spell +aspell +everywhere)                        ; tasing you for misspelling mispelling
  grammar                                            ; tasing grammar mistake every you make
 
