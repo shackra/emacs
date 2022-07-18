8f738bfd08204f7364b3c83ae0ed97148bb40b5d
Jorge Javier Araya Navarro <jorge@esavara.cr>
2022-07-17 23:57:50 -0600

    chg: Actualiza Doom y activa dirvish

--- packages.el
+++ packages.el
@@ -22,6 +22,8 @@
 
 (package! beacon)
 
+(unpin! dirvish)
+
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
