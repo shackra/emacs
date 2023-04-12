1e30b6993e91f915859ffaae492b6aa3fc6c9de3
Jorge Javier Araya Navarro <jorge@esavara.cr>
2023-04-11 18:10:43 -0600

    Traba code-review y closql a commits fijos
    - arreglo temporal para https://github.com/doomemacs/doomemacs/issues/7191

--- packages.el
+++ packages.el
@@ -9,6 +9,15 @@
 
 (package! ob-kotlin :recipe (:host github :repo "shackra/ob-kotlin" :files ("*.el")))
 
+; From https://github.com/iyefrat/doom-emacs/commit/bd944dc318efe2dfb00c1107ca6d70797dad1331
+; Due to https://github.com/doomemacs/doomemacs/issues/7191
+(package! code-review :recipe (:files ("graphql" "code-review*.el"))
+    :pin "26f426e99221a1f9356aabf874513e9105b68140")
+    ; HACK closql c3b34a6 breaks code-review wandersoncferreira/code-review#245,
+    ; and the current forge commit (but forge does have an upstream fix),
+    ; pinned as a temporary measure to prevent user breakages
+(package! closql :pin "0a7226331ff1f96142199915c0ac7940bac4afdd")
+
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
