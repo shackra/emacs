ee65ed467169d27fbca1ad37b12348e3ad99cbd1
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-03-05 10:12:19 -0600

    Desactiva ligatures y activa tabs

--- init.el
+++ init.el
@@ -20,18 +20,18 @@
        doom                                        ; what makes DOOM look the way it does
        doom-dashboard                              ; a nifty splash screen for Emacs
        ;;doom-quit                                 ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)                          ; 🙂
+       (emoji +unicode)                            ; 🙂
        hl-todo                                     ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides                             ; highlighted indent columns
-       (ligatures +extra)                          ; ligatures and symbols to make your code pretty again
-       minimap                                   ; show a map of the code on the side
+       ;;(ligatures +extra)                        ; ligatures and symbols to make your code pretty again
+       minimap                                     ; show a map of the code on the side
        modeline                                    ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash                                 ; blink cursor line after big motions
        ;;neotree                                   ; a project drawer, like NERDTree for vim
        ophints                                     ; highlight the region an operation acts on
        (popup +defaults +all)                      ; tame sudden yet inevitable temporary windows
-       ;;tabs                                      ; a tab bar for Emacs
+       tabs                                        ; a tab bar for Emacs
        (treemacs +lsp)                             ; a project drawer, like neotree but cooler
        unicode                                     ; extended unicode support for various languages
        (vc-gutter +pretty)                         ; vcs diff in the fringe
