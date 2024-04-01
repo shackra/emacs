fb7fbe32e98bf4957498b83da0084d85e43a186e
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-04-01 11:31:58 -0600

    Instala y activa hcl-mode

--- packages.el
+++ packages.el
@@ -17,6 +17,8 @@
 
 (package! just-mode :recipe (:host github :repo "leon-barrett/just-mode.el" :branch "main"))
 
+(package! hcl-mode :recipe (:host github :repo "hcl-emacs/hcl-mode"))
+
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
