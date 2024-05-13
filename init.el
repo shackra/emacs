430daaf157e99788f8b523387408f01936862525
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-05-13 13:58:03 -0600

    Nos regresamos a lsp-mode

--- init.el
+++ init.el
@@ -85,7 +85,7 @@
        (eval +overlay)                             ; run code, run (also, repls)
        ;;gist                                      ; interacting with github gists
        (lookup +dictionary +docsets +offline)      ; navigate your code and its documentation
-       (lsp +eglot)                                ; M-x vscode
+       (lsp +peek)                                 ; M-x vscode
        (magit +forge)                              ; a git porcelain for Emacs
        make                                        ; run make tasks from Emacs
        ;;pass                                      ; password manager for nerds
