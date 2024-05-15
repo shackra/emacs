1abd6a935b112f12fb9d763a8670eb9e02c03866
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-05-14 19:56:25 -0600

    Mueve partes de configuración de devdocs a config.el

--- modules/tools/devdocs/config.el
+++ modules/tools/devdocs/config.el
@@ -4,55 +4,8 @@
   :unless noninteractive
   :bind ("C-h D" . shackra/devdocs-lookup)
   :init
-  (add-to-list 'display-buffer-alist
-               '("\\*devdocs\\*"
-                 display-buffer-in-side-window
-                 (side . right)
-                 (slot . 3)
-                 (window-parameters . ((no-delete-other-windows . t)))
-                 (dedicated . t)))
-
   (defun shackra/devdocs-lookup (&optional ask-docs)
     "Light wrapper around `devdocs-lookup` which pre-populates the function input with thing at point"
     (interactive "P")
     (let ((query (thing-at-point 'symbol t)))
-      (devdocs-lookup ask-docs query)))
-  :config
-  (setq-hook! 'go-mode
-    devdocs-current-docs
-    '("go"
-      "sqlite"
-      "http"))
-  (setq-hook! 'gdscript-mode
-    devdocs-current-docs
-    '("godot~3.5"))
-  (setq-hook! 'emacs-lisp-mode
-    devdocs-current-docs
-    '("elisp"))
-  (setq-hook! 'python-mode
-    devdocs-current-docs
-    '("python~3.11"
-      "python~3.12"
-      "sqlite"
-      "flask~3.0"
-      "http"))
-  (setq-hook! 'web-mode
-    devdocs-current-docs
-    '("react"
-      "svelte"
-      "javascript"
-      "typescript"
-      "moment"
-      "tailwindcss"
-      "html"
-      "css"))
-  (setq-hook! 'typescript-mode
-    devdocs-current-docs
-    '("react"
-      "svelte"
-      "javascript"
-      "typescript"
-      "moment"
-      "tailwindcss"
-      "html"
-      "css")))
+      (devdocs-lookup ask-docs query))))
