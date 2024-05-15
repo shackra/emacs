a2b70a567750ba37a49529e827f317b0c54bd3ec
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-05-14 20:27:31 -0600

    Establece atajo usando `set-global-key`

--- modules/tools/devdocs/config.el
+++ modules/tools/devdocs/config.el
@@ -1,11 +1,63 @@
 ;;; tools/devdocs/config.el -*- lexical-binding: t; -*-
 
 (use-package! devdocs
-  :unless noninteractive
-  :bind ("C-h D" . shackra/devdocs-lookup)
   :init
   (defun shackra/devdocs-lookup (&optional ask-docs)
-    "Light wrapper around `devdocs-lookup` which pre-populates the function input with thing at point"
     (interactive "P")
     (let ((query (thing-at-point 'symbol t)))
-      (devdocs-lookup ask-docs query))))
+      (devdocs-lookup ask-docs query)))
+
+  (when (modulep! :tools devdocs)
+    (add-to-list 'display-buffer-alist
+                 '("\\*devdocs\\*"
+                   display-buffer-in-side-window
+                   (side . right)
+                   (slot . 3)
+                   (window-parameters . ((no-delete-other-windows . t)))
+                   (dedicated . t))))
+
+  (global-set-key (kbd "C-h D") 'shackra/devdocs-lookup)
+  :config
+  (setq-hook! 'go-mode-hook
+    devdocs-current-docs
+    '("go"
+      "sqlite"
+      "http"))
+
+  (setq-hook! 'gdscript-mode-hook
+    devdocs-current-docs
+    '("godot~3.5"))
+
+  (setq-hook! 'emacs-lisp-mode-hook
+    devdocs-current-docs
+    '("elisp"))
+
+  (setq-hook! 'python-mode-hook
+    devdocs-current-docs
+    '("python~3.11"
+      "python~3.12"
+      "sqlite"
+      "flask~3.0"
+      "http"))
+
+  (setq-hook! 'web-mode-hook
+    devdocs-current-docs
+    '("react"
+      "svelte"
+      "javascript"
+      "typescript"
+      "moment"
+      "tailwindcss"
+      "html"
+      "css"))
+
+  (setq-hook! 'typescript-mode-hook
+    devdocs-current-docs
+    '("react"
+      "svelte"
+      "javascript"
+      "typescript"
+      "moment"
+      "tailwindcss"
+      "html"
+      "css")))
