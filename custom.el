a6ab73823d395265ac42d862556633a46c047513
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-06-20 12:53:56 -0600

    Corrige cuando se agrega nuevo formateador

--- custom.el
+++ custom.el
@@ -7,7 +7,8 @@
    '("d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
-   '((go-tag-args "-transform" "camelcase")
+   '((+format-with . alejandra)
+     (go-tag-args "-transform" "camelcase")
      (git-commit-major-mode . git-commit-elisp-text-mode)
      (org-download-image-dir . "./imagenes")
      (org-clock-mode-line-total . today)))
