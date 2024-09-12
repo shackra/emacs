033a8e460f7b25be3ee04692da94eed569265706
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-09-11 18:44:45 -0600

    Ajuste menor
    registra cambio en la lista de paquetes seleccionados

--- init.el
+++ init.el
@@ -247,7 +247,7 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes doom-modeline projectile which-key)))
+ '(package-selected-packages '(shackle doom-themes doom-modeline projectile which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
