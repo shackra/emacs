1bf65022407c521ced931019137634bffe87d418
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-09-12 09:52:57 -0600

    Elimina configuración manual del fondo de Emacs

--- early-init.el
+++ early-init.el
@@ -32,9 +32,5 @@
                             (vertical-scroll-bars . nil)
                             (horizontal-scroll-bars . nil)
 
-                            ;; Setting the face in here prevents flashes of
-                            ;; color as the theme gets activated
-                            (background-color . "#000000")
-                            (ns-appearance . dark)
                             (ns-transparent-titlebar . t)))
 
