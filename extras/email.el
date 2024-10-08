67fc93ac1dcc061fe36b2681c8179a9c8545d8ed
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-10-08 15:14:46 -0600

    Copia macro y configura mu4e manualmente
    - Debido a mi ignorancia con mu4easy https://github.com/danielfleischer/mu4easy/issues/11

--- extras/email.el
+++ extras/email.el
@@ -1,64 +1,98 @@
-;;; Emacs Bedrock
-;;;
-;;; Extra config: Email
+(cl-defmacro shackra/mu4e-easy-context (&key c-name maildir mail smtp
+					     (sent-action 'sent)
+					     (name "Jorge Javier Araya Navarro")
+					     (sig message-signature))
+  (let
+      ((inbox      (concat "/" maildir "/Inbox"))
+       (sent       (concat "/" maildir "/Sent"))
+       (trash      (concat "/" maildir "/Trash"))
+       (refile     (concat "/" maildir "/Archive"))
+       (draft      (concat "/" maildir "/Drafts")))
 
-;;; Usage: Append or require this file from init.el for Email in Emacs. You will
-;;; need to do some heavy customization depending on your email provider.
+    `(make-mu4e-context
+      :name ,c-name
+      :match-func (lambda (msg)
+                    (when msg
+                      (string-match-p (concat "^/" ,maildir "/")
+                                      (mu4e-message-field msg :maildir))))
+      :vars '((user-mail-address		.	,mail)
+              (user-full-name			.	,name)
+              (mu4e-sent-folder			.	,sent)
+              (mu4e-drafts-folder		.	,draft)
+              (mu4e-trash-folder		.	,trash)
+              (mu4e-refile-folder		.	,refile)
+              (mu4e-compose-signature		.	(concat ,sig))
+              (mu4e-sent-messages-behavior	.	,sent-action)
+              (message-signature		.	,sig)
+              (mu4e-maildir-shortcuts .
+                                      ((,inbox   . ?i)
+                                       (,sent    . ?s)
+                                       (,trash   . ?t)
+                                       (,refile  . ?a)
+                                       (,draft   . ?d)))))))
 
-;;; Contents:
-;;;
-;;;  - Core Email Packages
-;;;  - Sample Setup: Gmail
-;;;  - Sample Setup: Fastmail
+(setq user-mail-address "jorge@esavara.cr")
+(setq message-signature "Jorge Araya\n\nContacto:\n Telegram: t.me/shackra · Signal: Shackra.28")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Core Email Packages
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun shackra/mu4e-compose-goodies ()
+  "Settings for mu4e compose mode."
+  (set-fill-column 120)
+  (turn-on-auto-fill)
+  (electric-indent-local-mode -1)
+  (turn-on-flyspell))
 
-(use-package mu4easy
-  :ensure t
+(use-package mu4e
   :bind ("C-c u" . mu4e)
-  :hook (after-init . mu4easy-mode)
+  :hook (mu4e-compose-mode . shackra/mu4e-compose-goodies)
   :custom
-  (mu4easy-signature "\nJorge Araya\n\nContacto:\n Telegram: t.me/shackra · Signal: Shackra.28")
+  (mu4e-maildir "~/Maildir")
+  (mu4e-get-mail-command (concat (executable-find "mbsync") " -a"))
   (mu4e-update-interval nil)
-  (mu4e-alert-email-notification-types '(count))
+  (mu4e-attachment-dir "~/Descargas")
+  (mu4e-change-filenames-when-moving t) ;; requerido por mbsync
   :config
-  (setq sendmail-program (executable-find "msmtp")
-	send-mail-function #'smtpmail-send-it
-	message-sendmail-extra-arguments '("--read-envelope-from")
-	message-send-mail-function #'message-send-mail-with-sendmail))
+  (setq sendmail-program (executable-find "msmtp"))
+  (setq message-sendmail-f-is-evil t)
+  (setq message-sendmail-extra-arguments '("--read-envelope-from"))
+  (setq message-send-mail-function #'message-send-mail-with-sendmail)
+  (setq send-mail-function #'message-send-mail-with-sendmail)
+  (setq message-signature "Jorge Araya\n\nContacto:\n Telegram: t.me/shackra · Signal: Shackra.28")
+  (setq mu4e-context-policy 'pick-first) ;; start with the first (default) context;
+  (setq mu4e-compose-context-policy 'ask) ;; ask for context if no context matches;
+  (setq message-kill-buffer-on-exit t)
+  ;; optional
+  ;; store link to message if in header view, not to header query:
+  (setq org-mu4e-link-query-in-headers-mode nil)
+  ;; don't have to confirm when quitting:
+  (setq mu4e-confirm-quit nil)
+  ;; number of visible headers in horizontal split view:
+  (setq mu4e-headers-visible-lines 5)
+  ;; hide annoying "mu4e Retrieving mail..." msg in mini buffer:
+  (setq mu4e-hide-index-messages t)
+  ;; customize the reply-quote-string:
+  (setq message-citation-line-format  "El %A %d de %B del %Y, %N escribió:\n")
+  ;; M-x find-function RET message-citation-line-format for docs:
+  (setq message-citation-line-function 'message-insert-formatted-citation-line)
+  ;; by default do not show related emails:
+  (setq mu4e-headers-include-related nil)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Sample Setup: Gmail
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(with-eval-after-load 'mu4easy
-  (setq mu4easy-contexts `(
-			   ,(mu4easy-context
-			     :c-name "personal"
-			     :maildir "principal"
-			     :mail "jorge@esavara.cr")
-			   ,(mu4easy-context
-			     :c-name  "gmail"
-			     :maildir "gmail"
-			     :mail    "shackrasislock0@gmail.com"
-			     :sent-action delete)
-			   ,(mu4easy-context
-			     :c-name "yahoo"
-			     :maildir "yahoo"
-			     :mail "jorgejavieran@yahoo.com.mx")
-			   ,(mu4easy-context
-			     :c-name "work"
-			     :maildir "work"
-			     :mail "jorge.araya@conductorone.com"
-			     :sig "Jorge Araya\nGolang Software Developer"
-			     :sent-action delete))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Sample Setup: Fastmail
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; contexts
+  (setq mu4e-contexts `(,(shackra/mu4e-easy-context
+			  :c-name "personal"
+			  :maildir "principal"
+			  :mail "jorge@esavara.cr")
+			,(shackra/mu4e-easy-context
+			  :c-name  "gmail"
+			  :maildir "gmail"
+			  :mail    "shackrasislock0@gmail.com"
+			  :sent-action delete)
+			,(shackra/mu4e-easy-context
+			  :c-name "yahoo"
+			  :maildir "yahoo"
+			  :mail "jorgejavieran@yahoo.com.mx")
+			,(shackra/mu4e-easy-context
+			  :c-name "work"
+			  :maildir "work"
+			  :mail "jorge.araya@conductorone.com"
+			  :sig "Jorge Araya\nGolang Software Developer"
+			  :sent-action delete))))
