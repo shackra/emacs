7971b02656dd3ec9b92d81ecde510107d52d18a6
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-10-08 06:50:14 -0600

    Cambia la firma por defecto y desactiva actualizaciones automaticas de mu4e

--- extras/email.el
+++ extras/email.el
@@ -21,13 +21,14 @@
   :ensure t
   :bind ("C-c u" . mu4e)
   :hook (after-init . mu4easy-mode)
+  :custom
+  (mu4easy-signature "\nJorge Araya\n\nContacto:\n Telegram: t.me/shackra · Signal: Shackra.28")
+  (mu4e-update-interval nil)
   :config
   (setq sendmail-program (executable-find "msmtp")
 	send-mail-function #'smtpmail-send-it
 	message-sendmail-extra-arguments '("--read-envelope-from")
 	message-send-mail-function #'message-send-mail-with-sendmail))
-
-(setq shackra-mu4e-signature "Jorge Araya\n\nContacto:\n Telegram: t.me/shackra · Signal: Shackra.28")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -39,18 +40,15 @@
 			   ,(mu4easy-context
 			     :c-name "personal"
 			     :maildir "personal"
-			     :mail (base64-decode-string "am9yZ2VAZXNhdmFyYS5jcg==")
-			     :sig ,shackra-mu4e-signature)
+			     :mail (base64-decode-string "am9yZ2VAZXNhdmFyYS5jcg=="))
 			   ,(mu4easy-context
 			     :c-name  "gmail"
 			     :maildir "gmail"
 			     :mail    (base64-decode-string "c2hhY2tyYXNpc2xvY2swQGdtYWlsLmNvbQ==")
-			     :sig ,shackra-mu4e-signature
 			     :sent-action delete)
 			   ,(mu4easy-context
 			     :c-name "yahoo"
 			     :maildir "yahoo"
-			     :sig ,shackra-mu4e-signature
 			     :mail (base64-decode-string "am9yZ2VqYXZpZXJhbkB5YWhvby5jb20ubXg="))
 			   ,(mu4easy-context
 			     :c-name "work"
@@ -58,7 +56,6 @@
 			     :mail (base64-decode-string "am9yZ2UuYXJheWFAY29uZHVjdG9yb25lLmNvbQ==")
 			     :sig "Jorge Araya\nGolang Software Developer"
 			     :sent-action delete))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;   Sample Setup: Fastmail
