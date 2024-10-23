552cbdd67dfb74e0ce71ed29bc65b78737cbcd1f
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-10-22 20:29:09 -0600

    Instala Crux

--- user.el
+++ user.el
@@ -326,3 +326,13 @@
   :ensure t
   :hook ((cmake-mode . cmake-ts-mode)
 	 ((cmake-mode cmake-ts-mode) . eglot-ensure)))
+
+(use-package crux
+  :ensure t
+  :config
+  (global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
+  (global-set-key (kbd "C-c o") #'crux-open-with)
+  (global-set-key [(shift return)] #'crux-smart-open-line)
+  (global-set-key (kbd "s-r") #'crux-recentf-find-file)
+  (global-set-key (kbd "C-<backspace>") #'crux-kill-line-backwards)
+  (global-set-key [remap kill-whole-line] #'crux-kill-whole-line))
