b95885920af88e72326ff7db425ff880edd7e86f
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-03-23 00:48:48 -0600

    Instala tempEl como paquete para plantillas

--- user.el
+++ user.el
@@ -428,3 +428,23 @@
 (use-package hl-todo
   :ensure t
   :config (global-hl-todo-mode 1))
+
+(use-package tempel
+  :preface
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+  :bind (("M-+" . tempel-complete)
+         ("M-*" . tempel-insert))
+  :hook
+  (conf-mode-hook . tempel-setup-capf)
+  (prog-mode-hook . tempel-setup-capf)
+  (text-mode-hook . tempel-setup-capf))
